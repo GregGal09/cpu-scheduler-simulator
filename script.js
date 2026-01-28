3b8794ce5e5f4f9e3318455e0158223aadcffb23
GregGal09 <galeri.gregorio@gmail.com>
2026-01-28 10:51:30 +0100

    aggiornamento

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 console.log('Happy developing ✨')
 
 // DICHIARAZIONE VARIABILI GLOBALI
-let p = ["p1", "p2", "p3"]; //ARRAY PROCESSI
-let at = ["2", "6", "8"]; //ARRAY TEMPO DI ARRIVO
-let bt = ["5", "3", "7"]; //ARRAY TEMPO DI BURST
-let rbt = ["3", "1","5"]; //ARRAY TEMPO DI BURST RIMANENTE
-let pr = ["5", "9", "8"]; //ARRAY PRIORITÀ
+let p = []; //ARRAY PROCESSI
+let at = []; //ARRAY TEMPO DI ARRIVO
+let bt = []; //ARRAY TEMPO DI BURST
+let rbt = []; //ARRAY TEMPO DI BURST RIMANENTE
+let pr = []; //ARRAY PRIORITÀ
+
+function addProcess() {
+   let pname = document.getElementById("pname").value;
+   let arrival = document.getElementById("arrival").value;
+   let burst = document.getElementById("burst").value;
+   let priority = document.getElementById("priority").value;
+    p.push(pname);
+   at.push(Number(arrival));
+   bt.push(Number(burst));
+   rbt.push(Number(burst));
+   pr.push(Number(priority));
+
+   document.getElementById("pname").value = "";
+   document.getElementById("arrival").value = "";
+   document.getElementById("burst").value = "";
+   document.getElementById("priority").value = "";
+   start()
+}
 
 /* FUNZIONE RESET
  La tabella dei processi viene sostituita con una tabella vuota
@@ -18,6 +36,12 @@ function reset(){
 
    tableEl.replaceChild(newTBodyEl, oldTBodyEl);
    document.getElementById("output").style.display = "none";
+
+   p=[];
+   at=[]; 
+   bt=[];
+   rbt=[];
+   pr=[];
 }
 /* FUNZIONE START
  si inseriscono nel corpo della tabella i dati dei processi  (nome, tempo di arrivo, tempo di burst, tempo di burst rimanente, priorità)  si mostra il diagramma di Gantt di attivazione dei processi
@@ -42,7 +66,26 @@ function start(){
        tdEl.appendChild(document.createTextNode(rbt[i]));
        tdEl = trEl.insertCell();
        tdEl.appendChild(document.createTextNode(pr[i]));
+       console.log(p); 
+
+
    }
    tableEl.replaceChild(newTBodyEl, oldTBodyEl);
    document.getElementById("output").style.display = "block";
+   function drawGanttFCFS() {
+   let gantt = document.getElementById("gantt");
+   gantt.innerHTML = "";
+
+      for (let i = 0; i < p.length; i++) {
+       let width = (bt[i] / totalBurst) * 100;
+
+       let block = document.createElement("div");
+       block.className = "progress-bar progress-bar-info";
+       block.style.width = width + "%";
+       block.innerText = p[i];
+
+       gantt.appendChild(block);
+   }
+}
+
 }
